c9f47defb82649b8d257fb9db4cb8faf378953a4
Nickonson <nikitabilan2002@gmail.com>
2021-12-09 00:57:58 +0300

    initial commit

--- Juja and Luja/Source.c
+++ Juja and Luja/Source.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void Luja(int *luja_size, int luja[][100], int luja_marks[][100], int x, int y);
+void juja(int *luja_size, int luja[][100], int luja_marks[][100], int x, int y);
 
 int main()
 {
-	int Luja_hights[100][100];
-	int Luja_marks[100][100];
+	int luja_hights[100][100];
+	int luja_marks[100][100];
 	int n;
 	scanf_s("%d", &n);
 
@@ -14,23 +14,23 @@ int main()
 	{
 		for (int j = 0; j < n; j++)
 		{
-			scanf_s("%d", &Luja_hights[i][j]);
+			scanf_s("%d", &luja_hights[i][j]);
 		}
 	}
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			Luja_marks[i][j] = 0;
+			luja_marks[i][j] = 0;
 
 
-	Luja(&n, Luja_hights, Luja_marks, 0, 0);
+	juja(&n, luja_hights, luja_marks, 0, 0);
 
-	//printing marks of Luja_marks
+	//printing marks of luja_marks
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			printf("  %d  ",Luja_marks[i][j]);
+			printf("  %d  ",luja_marks[i][j]);
 		}
 		printf("\n");
 	}
@@ -40,7 +40,7 @@ int main()
 
 }
 
-void Luja(int *luja_size, int luja[][100], int luja_marks[][100], int x, int y)
+void juja(int *luja_size, int luja[][100], int luja_marks[][100], int x, int y)
 {
 
 }
